a289d5777b0b3d60af84bdc95b11aef70122b115
Soofe <gasinski.przemek@gmail.com>
2015-12-13 17:35:01 +0100

    Implement Sign-in panel
    Get your shit together Matt & Javier

--- Blitz/WebContent/WEB-INF/jsp/login.jsp
+++ Blitz/WebContent/WEB-INF/jsp/login.jsp
@@ -30,17 +30,40 @@
 							</div>
 							<div class="panel-footer">
 								<!-- <button type="button" class="btn btn-default" data-dismiss="modal">Annuler</button> -->
-								<button type="submit" form="signup-form" class="btn btn-primary" id="signup-button">S'inscrire</button>
+								<button type="submit" form="signup-form" class="btn btn-primary" name="connection" value="signup">S'inscrire</button>
 							</div>
 						</div>
 					</div>
 
 					<div class="col-sm-5">
 						<!-- SIGN-IN -->
+						<div class="panel panel-default">
+							<div class="panel-heading">Connexion</div>
+							<div class="panel-body">
+								<form class="form" id="signin-form" role="form" method="post" action="login.html">
+									<!-- NICKNAME -->
+									<div class="form-group">
+										<label class="control-label" for="signin-nickname">Pseudo :</label> <input
+										type="text" class="form-control" name="signin-nickname">
+									</div>
+									<!-- PASSWORD -->
+									<div class="form-group">
+										<label class="control-label" for="signin-password">Mot
+											de Passe :</label> <input type="password" class="form-control"
+											name="signin-password">
+										</div>
+										<!-- REPEAT PASSWORD -->
+										</form>
+									</div>
+									<div class="panel-footer">
+										<!-- <button type="button" class="btn btn-default" data-dismiss="modal">Annuler</button> -->
+										<button type="submit" form="signin-form" class="btn btn-primary" name="connection" value="signin">Se Connecter</button>
+									</div>
+								</div>
+							</div>
+						</div>
+
 					</div>
-				</div>
 
-			</div>
-
-			<script src="lib/js/signup.js"></script>
-			<script src="lib/js/signin.js"></script>
+					<script src="lib/js/signup.js"></script>
+					<script src="lib/js/signin.js"></script>
--- Blitz/src/be/ipl/blitz/servlets/LoginServlet.java
+++ Blitz/src/be/ipl/blitz/servlets/LoginServlet.java
@@ -11,41 +11,88 @@ import javax.servlet.http.HttpSession;
 
 import be.ipl.blitz.usecases.UserUcc;
 import be.ipl.blitz.usecasesImpl.UserUccImpl;
+import be.ipl.blitz.utils.Util;
 
 @WebServlet("/login.html")
 public class LoginServlet extends HttpServlet {
+	private UserUcc userUcc;
+
+	@Override
+	public void init() throws ServletException {
+		super.init();
+		userUcc = new UserUccImpl();
+	}
+
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String nickname = req.getParameter("signup-nickname");
-		String password = req.getParameter("signup-password");
-		String passwordRepeat = req.getParameter("signup-repeat-password");
+		String connectionButton = req.getParameter("connection");
 
 		// TODO Needs a veeery big refactoring
-		String nextPage = "login.html";
+		// TODO Find a way to alert the user if there was an error
 
-		if (!nickname.isEmpty() && !password.isEmpty() && !passwordRepeat.isEmpty()) {
-			UserUcc userUcc = new UserUccImpl();
-			if (password.equals(passwordRepeat)) {
+		if (connectionButton == null) {
+
+		} else if (connectionButton.equals("signup")) {
+			String nickname = req.getParameter("signup-nickname");
+			String password = req.getParameter("signup-password");
+			String passwordRepeat = req.getParameter("signup-repeat-password");
+
+			try {
+				Util.checkObject(nickname);
+				Util.checkObject(password);
+				Util.checkObject(passwordRepeat);
+				Util.checkString(nickname);
+				Util.checkString(password);
+				Util.checkString(passwordRepeat);
+				if (password.equals(passwordRepeat)) {
+					try {
+						userUcc.saveUser(nickname, password);
+						login(nickname, req, resp);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			} catch (NullPointerException | IllegalArgumentException e) {
+				doGet(req, resp);
+				return;
+			}
+
+		} else if (connectionButton.equals("signin")) {
+			String nickname = req.getParameter("signin-nickname");
+			String password = req.getParameter("signin-password");
+
+			try {
+				Util.checkObject(nickname);
+				Util.checkObject(password);
+				Util.checkString(nickname);
+				Util.checkString(password);
 				try {
-					userUcc.saveUser(nickname, password);
+					userUcc.login(nickname, password);
+					login(nickname, req, resp);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
+			} catch (NullPointerException | IllegalArgumentException e) {
+				doGet(req, resp);
+				return;
 			}
 
-			HttpSession session = req.getSession();
-			synchronized (session) {
-				session.setAttribute("name", nickname);
-				session.setAttribute("connected", true);
-			}
-			nextPage = "index.html";
 		}
-		getServletContext().getNamedDispatcher(nextPage).forward(req, resp);
-
+		doGet(req, resp);
 	}
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		getServletContext().getNamedDispatcher("login.html").forward(req, resp);
+	}
+
+	private void login(String nickname, HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		HttpSession session = req.getSession();
+		synchronized (session) {
+			session.setAttribute("name", nickname);
+			session.setAttribute("connected", true);
+		}
+		getServletContext().getNamedDispatcher("index.html").forward(req, resp);
 	}
 }
