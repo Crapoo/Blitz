c7cb3905452a5ddaf7fc8799abbf739defc85344
Soofe <gasinski.przemek@gmail.com>
2015-12-13 14:02:31 +0100

    Improve Sign-up form & Add basic verifications in js

--- Blitz/WebContent/lib/js/signup.js
+++ Blitz/WebContent/lib/js/signup.js
@@ -4,6 +4,21 @@ $(document).ready(function() {
     var password = $("#signup-password").val();
     var passwordRepeat = $("#signup-repeat-password").val();
 
-    console.log(nickname + " " + password + " " + passwordRepeat);
+    var errorMessage = "";
+
+    if (!nickname || !password || !passwordRepeat) {
+      errorMessage = "Veuillez remplir tous les champs"
+    }
+    if (password != passwordRepeat) {
+      errorMessage = "Les mots de passe ne sont pas identiques"
+    }
+
+    // TODO Refactor
+
+    if (errorMessage) {
+      $("#signup-repeat-password").parent().after('<div class="alert alert-danger" role="alert"><strong><span class="glyphicon glyphicon-warning-sign" aria-hidden="true"></span></strong>&nbsp;&nbsp;' + errorMessage + '</div>');
+    } else {
+      $("#signup-repeat-password").parent().after('<div class="alert alert-success" role="alert"><strong><span class="glyphicon glyphicon-ok" aria-hidden="true"></span>&nbsp;&nbsp;</strong>Inscription complétée</div>');
+    }
   });
 });
