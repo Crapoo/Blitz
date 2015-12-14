1193c8561ce10f63154e9e8d85c123dca462dbb2
javier-lethe <javier.lethe@gmail.com>
2015-12-14 11:29:11 +0100

    GameUcc

---
+++ BlitzEJB/ejbModule/be/ipl/blitz/usecases/GameUcc.java
@@ -0,0 +1,20 @@
+package be.ipl.blitz.usecases;
+
+import java.util.List;
+
+public interface GameUcc {
+	boolean joinGame(String pseudo);
+	List<String> listPlayers();
+	boolean startGame();
+	String currentPlayer();
+	//List<Dice> listPlayedDices();
+	int throwDices();
+	boolean deleteDice(int numero);
+	int myScore();
+	int score(String pseudo);
+	boolean nextPlayer();
+	String winner();
+	boolean isOver();
+	void cancelGame();
+	//Etat getEtatPartie();
+}
