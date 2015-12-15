fce3368cfee35f4205de2258aeff58c2da48bd5b
Matteo Taroli <taroli.matteo@gmail.com>
2015-12-15 10:27:05 +0100

    Javadoc interfaces UCC

--- BlitzEJB/ejbModule/be/ipl/blitz/usecases/GameUcc.java
+++ BlitzEJB/ejbModule/be/ipl/blitz/usecases/GameUcc.java
@@ -1,26 +1,109 @@
 package be.ipl.blitz.usecases;
 
 import java.util.List;
+import java.util.Set;
 
 import javax.ejb.Remote;
 
+import be.ipl.blitz.domaine.Face;
 import be.ipl.blitz.domaine.Game;
 import be.ipl.blitz.domaine.Game.State;
+import be.ipl.blitz.domaine.User;
 
 @Remote
 public interface GameUcc {
 
-	boolean joinGame(String gameName, String pseudo);
+	/**
+	 * Ajoute la joueur désigné par son nom dans la partie donnée.
+	 * 
+	 * @param gameName
+	 *            Nom de la partie à rejoindre.
+	 * @param username
+	 *            Nom du joueur.
+	 * @return True si le joueur a pu rejoindre la partie.
+	 */
+	boolean joinGame(String gameName, String username);
+
+	/**
+	 * Crée un jeu avec le nom donnée et la date courante comme date de début.
+	 * 
+	 * @param gameName
+	 *            Nom de la partie à créer.
+	 * @return Vrai si la partie a pu être créée.
+	 */
 	boolean createGame(String gameName);
+
+	/**
+	 * Récupère l'état de la dernière partie lancée.
+	 * 
+	 * @return Etat de la partie ou null si aucune partie n'a été lancée jusqu'à
+	 *         maintenant.
+	 */
 	State getState();
+
+	/**
+	 * Démarre la partie avec les joueurs préalablement ajoutés.
+	 * 
+	 * @return True si la partie a pu être lancée.
+	 */
 	boolean startGame();
-	boolean isOver();
+
+	/**
+	 * Annule la partie précédemment lancée.
+	 */
 	void cancelGame();
-	boolean throwDice();
+
+	/**
+	 * Lance les dés du joueur courant.
+	 * 
+	 * @return Set de Face correspondant au résultat du lancer.
+	 */
+	Set<Face> throwDice();
+
+	/**
+	 * Récupère le dernière partie lancée.
+	 * 
+	 * @return La dernière partie lancée.
+	 */
 	Game getCurrentGame();
-	boolean deleteDie(int numero);
-	boolean nextPlayer();
+
+	/**
+	 * Supprime un certain nombre de dé du joueur donné. Si ce nombre est
+	 * supérieur au nombre de dés du joueur, supprime tous les dés.
+	 * 
+	 * @param numero
+	 *            Nombre de dé à supprimer.
+	 * @param username
+	 *            Nom du joueur à qui supprimer les dés.
+	 * @return True si les dés ont pu être supprimés.
+	 */
+	boolean deleteDice(int numero, String username);
+
+	/**
+	 * Change le joueur courant par le joueur devant joueur après.
+	 * 
+	 * @return Le nouveau joueur courant.
+	 */
+	User nextPlayer();
+
+	/**
+	 * Récupère le nom du gagnant de la dernière partie.
+	 * 
+	 * @return Le nom du gagnant.
+	 */
 	String winner();
+
+	/**
+	 * Récupère les noms de tous les joueurs participant à la partie courante.
+	 * 
+	 * @return Liste des noms de tous les joueurs.
+	 */
 	List<String> listPlayers();
+
+	/**
+	 * Récupère le joueur courant.
+	 * 
+	 * @return Le joueur courant.
+	 */
 	String getCurrentPlayer();
 }
