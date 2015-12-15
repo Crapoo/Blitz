2b2a19f744ba302de87e1701abc6bffceb621fa1
javier-lethe <javier.lethe@gmail.com>
2015-12-15 17:27:04 +0100

    added test project

---
+++ BlitzTest/src/be/ipl/blitz/usecasesImpl/TestGameUcc.java
@@ -0,0 +1,79 @@
+package be.ipl.blitz.usecasesImpl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestGameUcc {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testJoinGame() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testListPlayers() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStartGame() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCurrentPlayer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testThrowDice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteDice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNextPlayer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetWinner() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCancelGame() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateGame() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetState() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCurrentGame() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDrawCard() {
+		fail("Not yet implemented");
+	}
+
+}
