ee122bbd7bfdb1cdaa66d8c9a554b133e9adf782
Matteo Taroli <taroli.matteo@gmail.com>
2015-12-18 22:41:11 +0100

    Fix le timer

--- Blitz/WebContent/js/refresh-lobby.js
+++ Blitz/WebContent/js/refresh-lobby.js
@@ -5,25 +5,27 @@ function refresh() {
     dataType: "json",
   });
   $request.done(function (response, textStatus, xhr) {
-	  console.log("start game : " + response.startGame);
-	  if (response.startGame) {
-		  console.log("StartGame");
-      window.location.href = "board.html";
-    }
-    var playersList = "";
+	  console.log("start : " + response.startGame + " - cancel : " + response.cancelGame);
+  if (response.startGame) {
+	  window.location.href = "board.html";
+  } else if (response.cancelGame) {
+	  /* TODO : Matt - Ajouter un message pour expliquer à l'user le retour à l'index */
+	  window.location.href = "index.html";
+  }
+  var playersList = "";
 
-    $.each(response.playersList, function(i, player) {
-      playersList += '<li class="list-group-item">' + player + '</li>';
-    });
+  $.each(response.playersList, function(i, player) {
+	  playersList += '<li class="list-group-item">' + player + '</li>';
+   });
 
-    $('#lobby-players-list').html(playersList);
-    $('#players-count').text(response.playersCount);
-    console.log(response);
-  });
-  $request.fail(function (xhr, textStatus, errorThrown) {
-    //alert(errorThrown);
-    console.log(errorThrown);
-  });
+   $('#lobby-players-list').html(playersList);
+   $('#players-count').text(response.playersCount);
+   // console.log(response);
+ });
+ $request.fail(function (xhr, textStatus, errorThrown) {
+   //alert(errorThrown);
+   console.log(errorThrown);
+ });
 }
 
 $(function () {
--- Blitz/src/be/ipl/blitz/servlets/RefreshLobbyServlet.java
+++ Blitz/src/be/ipl/blitz/servlets/RefreshLobbyServlet.java
@@ -9,12 +9,14 @@ import javax.ejb.EJB;
 import javax.json.Json;
 import javax.json.JsonArrayBuilder;
 import javax.json.JsonObjectBuilder;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import be.ipl.blitz.domaine.Game.State;
 import be.ipl.blitz.usecases.GameUcc;
 
 @WebServlet("/refresh-lobby.html")
@@ -22,7 +24,9 @@ public class RefreshLobbyServlet extends HttpServlet {
 	@EJB
 	private GameUcc gameUcc;
 
-	private boolean startGame = false;
+	private boolean timerRunning = false;
+	private boolean startGame;
+	private boolean cancelGame;
 
 	private static final long serialVersionUID = 1L;
 
@@ -33,31 +37,44 @@ public class RefreshLobbyServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		JsonObjectBuilder oBuilder = Json.createObjectBuilder();
-		JsonArrayBuilder aBuilder = Json.createArrayBuilder();
-		List<String> players = gameUcc.listPlayers();
+		final ServletContext ctx = getServletContext();
+		synchronized (ctx) {
+			JsonObjectBuilder oBuilder = Json.createObjectBuilder();
+			JsonArrayBuilder aBuilder = Json.createArrayBuilder();
+			List<String> players = gameUcc.listPlayers();
 
-		if (players.size() == 1) {
-			Timer timer = new Timer();
-			TimerTask task = new TimerTask() {
+			if (!timerRunning && gameUcc.getState() == State.INITIAL) {
+				startGame = false;
+				cancelGame = false;
+				timerRunning = true;
+				Timer timer = new Timer();
+				TimerTask task = new TimerTask() {
 
-				@Override
-				public void run() {
-					System.out.println("en effet, joueurs : " + gameUcc.listPlayers().size() + "/" + gameUcc.getMinPlayers());
-					startGame = gameUcc.listPlayers().size() >= gameUcc.getMinPlayers();
-				}
-			};
-			timer.schedule(task, 30000);
+					@Override
+					public void run() {
+						if (gameUcc.listPlayers().size() >= gameUcc.getMinPlayers()) {
+							startGame = true;
+						} else {
+							System.out.println("cancel");
+							gameUcc.cancelGame();
+							cancelGame = true;
+							timerRunning = false;
+						}
+					}
+				};
+				timer.schedule(task, 5000);
+			}
+			
+			for (String player : players) {
+				aBuilder.add(player);
+			}
+
+			oBuilder.add("playersList", aBuilder);
+			oBuilder.add("playersCount", players.size());
+			oBuilder.add("minPlayers", gameUcc.getMinPlayers());
+			oBuilder.add("startGame", startGame);
+			oBuilder.add("cancelGame", cancelGame);
+			response.getWriter().print(oBuilder.build().toString());
 		}
-		for (String player : players) {
-			aBuilder.add(player);
-		}
-
-		oBuilder.add("playersList", aBuilder);
-		oBuilder.add("playersCount", players.size());
-		oBuilder.add("minPlayers", gameUcc.getMinPlayers());
-		oBuilder.add("startGame", startGame);
-		response.getWriter().print(oBuilder.build().toString());
 	}
-
 }
