4c88683bd12b45bfb68e44770d5edb4e6d40a1f0
Soofe <gasinski.przemek@gmail.com>
2015-12-18 14:50:05 +0100

    Fix some shit

--- Blitz/WebContent/js/game/initialise-board.js
+++ Blitz/WebContent/js/game/initialise-board.js
@@ -38,12 +38,16 @@ function createEnemy(username, nbCards, nbDice, avatarPath) {
 	var enemy = $('<div class="enemy col-md-3" id="' + username + '">');
 	enemy.append('<div class="col-md-4 avatar"><img src="" alt="" class="img-responsive"><h3>' +
 	username + '</h3></div>');
+
 	var infoPanel = $('<div class="col-md-8 info-panel">');
 	var infoRow = $('<div class="row info">');
+
 	infoRow.append('<p>Cartes : <span class="nb-cards">' + nbCards +
 	'</span></p><p>D&eacute;s : <span class="nb-dice">' + nbDice +
 	'</span></p>');
+
 	var buttons = $('<div class="row buttons">');
+	
 	infoPanel.append(infoRow);
 	infoPanel.append(buttons);
 
